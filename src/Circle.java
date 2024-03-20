f9054d3fa231aac640128f7ed26181ebe41a6119
kimsj1121 <tjdwls001121@gmail.com>
2024-03-20 20:42:15 +0900

    first commit

--- src/Circle.java
+++ src/Circle.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Circle {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         final double PI = 3.14;
@@ -12,3 +12,9 @@ public class Circle {
         System.out.println("원의 넓이:"+(PI*radius*radius));//intellij git commmit push 알려주실분 구함~~~~--->>>>>>>>
     }
 }
+/* 야이 컴퓨터 개색갸
+어디서 사람을 이겨먹으려 해?
+내가 오류가 있어도 github에 push and commit 하겠다니까?
+야이 시팔롬아
+intellij sibar
+* */
