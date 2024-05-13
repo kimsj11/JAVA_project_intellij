9cce044f4e123a26d2757e52fdee21e6c8fa7e40
kimsj1121 <tjdwls001121@gmail.com>
2024-05-13 22:03:47 +0900

    asdfasdfasdfasdfasdf

--- src/AccessAndStatic/SingletonBasic.java
+++ src/AccessAndStatic/SingletonBasic.java
@@ -9,15 +9,15 @@ class Singleton {
 
     public static Singleton getInstance() {
         if (one == null) {
-            return new Singleton();
-        } else {
-            return one;
+            one = new Singleton();
         }
+            return one;
     }
 }
 
 public class SingletonBasic {
     public static void main(String[] args) {
         Singleton singleton = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
     }
 }
