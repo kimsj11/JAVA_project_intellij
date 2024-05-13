0db1070d64d0deb7f96620a9068a11a7381c6cf6
kimsj1121 <tjdwls001121@gmail.com>
2024-05-13 22:01:25 +0900

    feat: SingletonBasic

---
+++ src/AccessAndStatic/SingletonBasic.java
@@ -0,0 +1,23 @@
+package AccessAndStatic;
+
+class Singleton {
+
+    private static Singleton one;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (one == null) {
+            return new Singleton();
+        } else {
+            return one;
+        }
+    }
+}
+
+public class SingletonBasic {
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+    }
+}
